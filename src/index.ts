494cdb026fc7e0e3cf27e55395d9bbca40cf78af
maxilipi <104212047+maxilipi@users.noreply.github.com>
2022-04-27 17:32:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let vuelta1: number = 100;
+let vuelta2: number = 150;
+let vuelta3: number = 100;
+let vuelta4: number = 200;
+let carreracompleta: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+
+console.log("la primera vuelta duro", vuelta1,"minutos");
+console.log("la segunda vuelta duro", vuelta2, "minutos");
+console.log("la tercera vuelta duro", vuelta3); "minutos");
+console.log("la cuarta vuelta duro", vuelta4); "minutos");
+console.log("duro en total", carreracompleta, "minutos");
+console.log("y el promedio de cada vuelta es", carreracompleta/4);
+
+
